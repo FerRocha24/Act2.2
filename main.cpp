809409099b5fc01c82e94fbe2d6b5e77e0e68a19
Fernando Rocha <111387670+FerRocha24@users.noreply.github.com>
2025-03-31 20:06:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
-// Fernando Rocha Martínez A00836634
-
 /*
-Instrucciones:
-Utilizando la técnica de programación de "algoritmos avaros" y "programación dinámica",
-escribe en equipos de máximo dos personas, en C++ un programa que resuelve el problema del cambio de monedas.
-
-El programa recibe un número entero N, seguido de N valores enteros (uno en cada línea)
-que representan las diferentes denominaciones disponibles de las monedas.
-Seguido de esto, el programa recibe dos números enteros: P y Q,
-(uno en cada línea) por la entrada estándar,
-que representan P: el precio de un producto dado y Q: es el billete o moneda con el que se paga dicho producto.
-
-La salida del programa es una lista de N valores correspondientes
-al número de monedas de cada denominación, de mayor a menor, una en cada línea,
-que se tienen que dar para dar el cambio por el producto pagado,
-primero utilizando programación dinámica y luego utilizando un algoritmo avaro.
-
-Complejidad computacional:
-- El algoritmo de programación dinámica tiene una complejidad de O(N * cambio), donde:
-    - N es la cantidad de denominaciones de monedas.
-    - cambio es la cantidad a devolver.
-- El algoritmo avaro tiene una complejidad de O(N), ya que recorre las denominaciones una sola vez.
-
-Correr con:
-g++ -std=c++17 -o main main.cpp
-./main
-
+* Fernando Rocha Martínez A00836634
+*
+* Instrucciones:
+* Utilizando la técnica de programación de "algoritmos avaros" y "programación dinámica",
+* escribe en equipos de máximo dos personas, en C++ un programa que resuelve el problema del cambio de monedas.
+*
+* El programa recibe un número entero N, seguido de N valores enteros (uno en cada línea)
+* que representan las diferentes denominaciones disponibles de las monedas.
+* Seguido de esto, el programa recibe dos números enteros: P y Q,
+* (uno en cada línea) por la entrada estándar,
+* que representan P: el precio de un producto dado y Q: es el billete o moneda con el que se paga dicho producto.
+*
+* La salida del programa es una lista de N valores correspondientes
+* al número de monedas de cada denominación, de mayor a menor, una en cada línea,
+* que se tienen que dar para dar el cambio por el producto pagado,
+* primero utilizando programación dinámica y luego utilizando un algoritmo avaro.
+*
+* Complejidad computacional:
+* - El algoritmo de programación dinámica tiene una complejidad de O(N * cambio), donde:
+*   - N es la cantidad de denominaciones de monedas.
+*    - cambio es la cantidad a devolver.
+* - El algoritmo avaro tiene una complejidad de O(N), ya que recorre las denominaciones una sola vez.
+*
+* Correr con:
+* g++ -std=c++17 -o main main.cpp
+* ./main
+*
 */
 
 #include <iostream>
@@ -41,19 +41,15 @@ using namespace std;
  * @param cambio Monto de cambio a devolver.
  * @return Vector con el número de monedas de cada denominación.
  */
-vector<int> cambioDinamico(const vector<int> &denominaciones, int cambio)
-{
+void cambio_Dinamico(const vector<int>& denominaciones, int cambio) {
     int n = denominaciones.size();
     vector<int> dp(cambio + 1, INT_MAX);
     vector<int> seleccion(cambio + 1, -1);
     dp[0] = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = denominaciones[i]; j <= cambio; j++)
-        {
-            if (dp[j - denominaciones[i]] != INT_MAX && dp[j - denominaciones[i]] + 1 < dp[j])
-            {
+    for (int i = 0; i < n; i++) {
+        for (int j = denominaciones[i]; j <= cambio; j++) {
+            if (dp[j - denominaciones[i]] != INT_MAX && dp[j - denominaciones[i]] + 1 < dp[j]) {
                 dp[j] = dp[j - denominaciones[i]] + 1;
                 seleccion[j] = i;
             }
@@ -61,12 +57,13 @@ vector<int> cambioDinamico(const vector<int> &denominaciones, int cambio)
     }
 
     vector<int> resultado(n, 0);
-    if (dp[cambio] == INT_MAX)
-        return resultado;
+    if (dp[cambio] == INT_MAX) {
+        cout << "No se puede dar cambio con las denominaciones dadas." << endl;
+        return;
+    }
 
     int tempCambio = cambio;
-    while (tempCambio > 0)
-    {
+    while (tempCambio > 0) {
         int index = seleccion[tempCambio];
         if (index == -1)
             break;
@@ -74,7 +71,9 @@ vector<int> cambioDinamico(const vector<int> &denominaciones, int cambio)
         tempCambio -= denominaciones[index];
     }
 
-    return resultado;
+    for (int i = 0; i < n; i++) {
+        cout << "Monedas de " << denominaciones[i] << ": " << resultado[i] << endl;
+    }
 }
 
 /**
@@ -83,18 +82,18 @@ vector<int> cambioDinamico(const vector<int> &denominaciones, int cambio)
  * @param cambio Monto de cambio a devolver.
  * @return Vector con el número de monedas de cada denominación.
  */
-vector<int> cambioAvaro(const vector<int> &denominaciones, int cambio)
-{
+void cambio_Avaro(const vector<int>& denominaciones, int cambio) {
     int n = denominaciones.size();
     vector<int> resultado(n, 0);
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         resultado[i] = cambio / denominaciones[i];
         cambio %= denominaciones[i];
     }
 
-    return resultado;
+    for (int i = 0; i < n; i++) {
+        cout << "Monedas de " << denominaciones[i] << ": " << resultado[i] << endl;
+    }
 }
 
 int main()
@@ -131,14 +130,14 @@ int main()
     {
         int cambio = Q - P;
         cout << "Cambio usando programacion dinamica:" << endl;
-        vector<int> resultadoDinamico = cambioDinamico(denominaciones, cambio);
+        vector<int> resultadoDinamico = cambio_Dinamico(denominaciones, cambio);
         for (int i = 0; i < N; i++)
         {
             cout << resultadoDinamico[i] << " moneda(s) de " << denominaciones[i] << endl;
         }
 
         cout << "\nCambio usando algoritmo avaro:" << endl;
-        vector<int> resultadoAvaro = cambioAvaro(denominaciones, cambio);
+        vector<int> resultadoAvaro = cambio_Avaro(denominaciones, cambio);
         for (int i = 0; i < N; i++)
         {
             cout << resultadoAvaro[i] << " moneda(s) de " << denominaciones[i] << endl;
